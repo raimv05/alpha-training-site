f85c07577531a84c6474745b38ef1546ff1cac0b
raimv05 <manish.rai5300@gmail.com>
2026-05-20 12:19:58 +0530

    UI Feature: Add Google Maps embed and link for Alpha IT location in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="premium_footer">
       <div className="container">
         <div className="row">
-          <div className="col-sm-6 col-lg-4">
+          <div className="col-sm-6 col-lg-3">
             <div className="footer_block">
               <img src="/img/logo.png" className="footer_logo" alt="Logo" />
               <p className="footer_about">
@@ -37,7 +37,33 @@ export default function Footer() {
               </ul>
             </div>
           </div>
-          <div className="col-sm-6 col-lg-4">
+          <div className="col-sm-6 col-lg-2">
+            <div className="footer_block">
+              <h4 className="footer_title">Our Location</h4>
+              <div className="footer_map_wrapper">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3430.73024844393!2d76.72481197630444!3d30.68241347460981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390febfe87e901c3%3A0xaf2bcb5f0c8e1abf!2sAlpha%20IT%20Managed%20Services%20Private%20Limited!5e0!3m2!1sen!2sin!4v1716187900000!5m2!1sen!2sin"
+                  width="100%"
+                  height="110"
+                  style={{ border: 0, borderRadius: "10px" }}
+                  allowFullScreen=""
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                  title="Alpha IT Location"
+                  className="footer_map_iframe"
+                ></iframe>
+                <a
+                  href="https://maps.app.goo.gl/bQK2rxM5ebeWMCJ96"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="footer_map_link"
+                >
+                  <span className="map_icon">📍</span> Find us on Maps
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 col-lg-3">
             <div className="footer_block">
               <h4 className="footer_title">Stay Updated</h4>
               <p className="footer_about">Subscribe to our newsletter for latest updates and offers.</p>
