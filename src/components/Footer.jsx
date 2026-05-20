f884aaa9f4895ee9747a027133bdad7dca62b2ce
raimv05 <manish.rai5300@gmail.com>
2026-05-20 12:53:45 +0530

    UI Update: Change website link in footer from alphait.us to alphaitms.com

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -88,8 +88,8 @@ export default function Footer() {
                         <line x1="2" y1="12" x2="22" y2="12"></line>
                         <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
                       </svg>
-                      <a href="https://alphait.us" target="_blank" rel="noopener noreferrer" className="contact_text contact_link">
-                        alphait.us
+                      <a href="https://alphaitms.com" target="_blank" rel="noopener noreferrer" className="contact_text contact_link">
+                        alphaitms.com
                       </a>
                     </div>
                     <div className="contact_item">
